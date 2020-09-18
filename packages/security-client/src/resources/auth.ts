106e6556794f4b700512301e1d05c5a4f13bc015
Marcin Kopa <marcin.info@gmail.com>
2020-09-18 15:15:22 +0200

    Update resources

--- packages/security-client/src/resources/auth.ts
+++ packages/security-client/src/resources/auth.ts
@@ -11,69 +11,68 @@ import {
   ResetPasswordResponse,
 } from "../defs/auth";
 
-export class AuthResources implements Auth {
-  constructor(private serviceClient: ServiceClient) {}
+export const auth = (serviceClient: ServiceClient) =>
+  ({
+    /**
+     * Returns Token object
+     *
+     *
+     * @param {Promise<Token>} request - LoginRequest
+     * @returns Token
+     *
+     * @beta
+     */
+    login(request: LoginRequest): Promise<Token> {
+      return serviceClient
+        .request({
+          pathname: "/api/public/auth/login",
+          method: "POST",
+          body: JSON.stringify(request),
+        })
+        .then((response) => response!.body as Token)
+        .catch((error) => {
+          throw getHttpError(error);
+        });
+    },
 
-  /**
-   * Returns Token object
-   *
-   *
-   * @param {Promise<Token>} request - LoginRequest
-   * @returns Token
-   *
-   * @beta
-   */
-  login(request: LoginRequest): Promise<Token> {
-    return this.serviceClient
-      .request({
-        pathname: "/api/public/auth/login",
-        method: "POST",
-        body: JSON.stringify(request),
-      })
-      .then((response) => response!.body as Token)
-      .catch((error) => {
-        throw getHttpError(error);
-      });
-  }
+    resetPassword(request: ResetPasswordRequest): Promise<ResetPasswordResponse> {
+      return serviceClient
+        .request({
+          pathname: `/api/users/reset-password/${request.resetPasswordToken}`,
+          method: "POST",
+          body: JSON.stringify({
+            newPassword: request.newPassword,
+          }),
+        })
+        .then((response) => response.body as ResetPasswordResponse)
+        .catch((error) => {
+          throw getHttpError(error);
+        });
+    },
 
-  resetPassword(request: ResetPasswordRequest): Promise<ResetPasswordResponse> {
-    return this.serviceClient
-      .request({
-        pathname: `/api/users/reset-password/${request.resetPasswordToken}`,
-        method: "POST",
-        body: JSON.stringify({
-          newPassword: request.newPassword,
-        }),
-      })
-      .then((response) => response.body as ResetPasswordResponse)
-      .catch((error) => {
-        throw getHttpError(error);
-      });
-  }
+    refreshToken(request: RefreshTokenRequest): Promise<RefreshTokenResponse> {
+      return serviceClient
+        .request({
+          pathname: "/api/public/auth/refresh-token",
+          method: "POST",
+          body: JSON.stringify(request),
+        })
+        .then((response) => response.body as RefreshTokenResponse)
+        .catch((error) => {
+          throw getHttpError(error);
+        });
+    },
 
-  refreshToken(request: RefreshTokenRequest): Promise<RefreshTokenResponse> {
-    return this.serviceClient
-      .request({
-        pathname: "/api/public/auth/refresh-token",
-        method: "POST",
-        body: JSON.stringify(request),
-      })
-      .then((response) => response.body as RefreshTokenResponse)
-      .catch((error) => {
-        throw getHttpError(error);
-      });
-  }
-
-  refreshUserActiveToken(userId: string): Promise<RefreshUserActiveTokenResponse> {
-    return this.serviceClient
-      .request({
-        pathname: "/api/public/auth/refresh-token",
-        method: "POST",
-        body: JSON.stringify({ userId }),
-      })
-      .then((response) => response.body as RefreshUserActiveTokenResponse)
-      .catch((error) => {
-        throw getHttpError(error);
-      });
-  }
-}
+    refreshUserActiveToken(userId: string): Promise<RefreshUserActiveTokenResponse> {
+      return serviceClient
+        .request({
+          pathname: "/api/public/auth/refresh-token",
+          method: "POST",
+          body: JSON.stringify({ userId }),
+        })
+        .then((response) => response.body as RefreshUserActiveTokenResponse)
+        .catch((error) => {
+          throw getHttpError(error);
+        });
+    },
+  } as Auth);
--- packages/security-client/src/resources/policy.ts
+++ packages/security-client/src/resources/policy.ts
@@ -10,43 +10,42 @@ import {
   RemovePolicyRequest,
 } from "../defs/policy";
 
-export class PolicyResources implements Policy {
-  constructor(private serviceClient: ServiceClient) {}
+export const policy = (serviceClient: ServiceClient) =>
+  ({
+    addPolicy(request: AddPolicyRequest): Promise<AddPolicyResponse> {
+      return serviceClient
+        .request({
+          pathname: "/api/policy/add-policy",
+          method: "POST",
+          body: JSON.stringify({ ...request }),
+        })
+        .then((response) => response.body as AddPolicyResponse)
+        .catch((error) => {
+          throw getHttpError(error);
+        });
+    },
 
-  addPolicy(request: AddPolicyRequest): Promise<AddPolicyResponse> {
-    return this.serviceClient
-      .request({
-        pathname: "/api/policy/add-policy",
-        method: "POST",
-        body: JSON.stringify({ ...request }),
-      })
-      .then((response) => response.body as AddPolicyResponse)
-      .catch((error) => {
-        throw getHttpError(error);
-      });
-  }
+    getPolicies(queryFilter: GetPoliciesRequest = {}): Promise<GetPoliciesResponse> {
+      return serviceClient
+        .request({
+          pathname: `/api/policy/get-policies?${qs.stringify(queryFilter)}`,
+          method: "GET",
+        })
+        .then((response) => response.body as GetPoliciesResponse)
+        .catch((error) => {
+          throw getHttpError(error);
+        });
+    },
 
-  getPolicies(queryFilter: GetPoliciesRequest = {}): Promise<GetPoliciesResponse> {
-    return this.serviceClient
-      .request({
-        pathname: `/api/policy/get-policies?${qs.stringify(queryFilter)}`,
-        method: "GET",
-      })
-      .then((response) => response.body as GetPoliciesResponse)
-      .catch((error) => {
-        throw getHttpError(error);
-      });
-  }
-
-  async removePolicy(request: RemovePolicyRequest): Promise<void> {
-    await this.serviceClient
-      .request({
-        pathname: "/api/policy/remove-policy",
-        method: "DELETE",
-        query: request,
-      })
-      .catch((error) => {
-        throw getHttpError(error);
-      });
-  }
-}
+    async removePolicy(request: RemovePolicyRequest): Promise<void> {
+      await serviceClient
+        .request({
+          pathname: "/api/policy/remove-policy",
+          method: "DELETE",
+          query: request,
+        })
+        .catch((error) => {
+          throw getHttpError(error);
+        });
+    },
+  } as Policy);
